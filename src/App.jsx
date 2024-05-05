f2b642396d12ecab959c24213143cbaaa4515fdb
Akash Gupta <officialmycollege@gmail.com>
2024-05-05 23:31:13 +0530

    Intialize project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import InfiniteScroll from "./components/InfiniteScroll";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <InfiniteScroll />
+    </div>
+  );
+};
+
+export default App;
