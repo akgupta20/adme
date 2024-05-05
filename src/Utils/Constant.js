ae31a87ead20f23abd92eef895a646f5e9dfa6b0
Akash Gupta <officialmycollege@gmail.com>
2024-05-05 23:54:44 +0530

    Navbar created

---
+++ src/Utils/Constant.js
@@ -0,0 +1,26 @@
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    url: "#",
+  },
+  {
+    id: 2,
+    name: "About",
+    url: "#",
+  },
+  {
+    id: 3,
+    name: "Services",
+    url: "#",
+  },
+  {
+    id: 4,
+    name: "Contact",
+    url: "#",
+  },
+];
+
+const siteTitle = "Akash Gupta - aDMe";
+
+export { navLinks, siteTitle };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import styles from "./Navbar.module.css";
+import { siteTitle, navLinks } from "../Utils/Constant";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className={styles.navbar}>
+      <div>
+        <img
+          src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png"
+          width="30"
+          height="30"
+          alt="Logo"
+          className={styles.navbarLogo}
+        />
+      </div>
 
-export default Navbar
+      <h2 className={styles.title}>{siteTitle}</h2>
+      <ul className={styles.navbarLinks}>
+        {navLinks.map((link) => {
+          return (
+            <li className={styles.navbarLink}>
+              <a href="#">{link.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
